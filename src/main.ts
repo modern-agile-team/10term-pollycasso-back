88bc37e20ee856f4984818c75c48834e46a8ee86
LDH <leedoh03@naver.com>
2025-10-01 17:20:00 +0900

    refactor: (#5) salt rounds에 클래스 속성 사용 및 주석 제거

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
-  console.error('❌ 서버 시작 중 오류 발생:', err);
-  process.exit(1); // ✅ 프로세스 강제 종료 (중요)
+  console.error('서버 시작 중 오류 발생:', err);
+  process.exit(1);
 });
