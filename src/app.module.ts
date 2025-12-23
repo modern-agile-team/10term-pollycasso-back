c0c8829bb41557fe0fa2d5338e40715b589c661f
YJE <c0d1ngyje@gmail.com>
2025-12-23 17:45:01 +0900

    fix: (#57) 경로 문제로 인한 빌드 오류 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoomsModule } from './rooms/room.module';
+import { RoomsModule } from './room/room.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { ChatModule } from './chat/chat.module';
