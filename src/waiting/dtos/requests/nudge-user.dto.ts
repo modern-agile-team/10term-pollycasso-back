8383b233fbf692e046071b0c1b300e4e737c3c43
younjieun <c0d1ngyje@gmail.com>
2026-01-23 15:30:32 +0900

    Revert "service와 gateway 책임 분리 및 DTO 개선"

--- src/waiting/dtos/requests/nudge-user.dto.ts
+++ src/waiting/dtos/requests/nudge-user.dto.ts
@@ -1,9 +1,8 @@
-import { IsInt, Min } from 'class-validator';
+import { IsInt } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class NudgeUserDto {
   @IsInt()
-  @Min(1)
   @ApiProperty({
     description: '재촉할 플레이어 ID',
     example: 2,
